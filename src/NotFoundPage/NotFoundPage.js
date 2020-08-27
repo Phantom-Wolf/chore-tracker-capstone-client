b04216c490bc1fcd5575df3011407903dbf3b9be
Mitchell Schmidt <mitchellt.s@outlook.com>
2020-08-26 20:13:36 -0500

    added image to 404 page and changed some fonts

--- src/NotFoundPage/NotFoundPage.js
+++ src/NotFoundPage/NotFoundPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./NotFoundPage.css";
 import CategoryListNav from "../CategoryListNav/CategoryListNav";
 import ChoreContext from "../ChoreContext";
+import error404 from "../error404.jpg"
 
 export class NotFoundPage extends Component {
 	static contextType = ChoreContext;
@@ -15,10 +16,9 @@ export class NotFoundPage extends Component {
 				<nav>
 					<CategoryListNav />
 				</nav>
-				<p>
-					Opps! Looks like we got lost. No worries, press any link on the navigation bar to get back
-					on track.
-				</p>
+				<div className="notFoundBody">
+				<img className="errorImg" src={error404} alt="error comic meme"/> 
+				</div>
 			</div>
 		);
 	}
